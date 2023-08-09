91f946856510950fa6e4e6d11aa08a0697560169
Dean Brittain <deanbrittain@Deans-MacBook-Pro.local>
2023-08-09 14:17:01 -0600

    Setup toolbar, install tng custom font, setup routing, add additional components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EngineeringComponent } from './components/engineering/engineering.component';
+import { CrewComponent } from './components/crew/crew.component';
+import { SuppliesComponent } from './components/supplies/supplies.component';
+import { MissionControlComponent } from './components/mission-control/mission-control.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'crew', component: CrewComponent },
+  { path: 'engineering', component: EngineeringComponent },
+  { path: 'supplies', component: SuppliesComponent },
+  { path: 'mission-control', component: MissionControlComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Default redirect to the dashboard
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CrewComponent } from './components/crew/crew.component';
+import { EngineeringComponent } from './components/engineering/engineering.component';
+import { SuppliesComponent } from './components/supplies/supplies.component';
+import { MissionControlComponent } from './components/mission-control/mission-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CrewComponent,
+    EngineeringComponent,
+    SuppliesComponent,
+    MissionControlComponent,
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
